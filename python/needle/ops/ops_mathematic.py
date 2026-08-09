d51ca6b92ba67181da605e84c56c8b5e3e63e69e
AvailableUsernameA <huc014@ucsd.edu>
2026-08-09 13:45:57 -0400

    fix stack (str has no Array)

--- python/needle/ops/ops_mathematic.py
+++ python/needle/ops/ops_mathematic.py
@@ -605,9 +605,12 @@ def fft2d_recurse(inp2d):
     _, H, W = inp2d.shape
     inp2d = array_api.reshape(inp2d.compact(), (H, W))
     row_fft = array_api.empty((H, W), dtype="complex32", device=inp2d.device)
+    print("row_fft:", type(row_fft._handle))
     for r in range(H):
         inp1d = array_api.reshape(inp2d[r, :].compact(), (W,))
-        row_fft[r, :] = fft1d_recurse(inp1d)
+        rr = fft1d_recurse(inp1d)
+        print(type(rr._handle))
+        row_fft[r, :] = rr
     out = array_api.empty((H, W), dtype="complex32", device=inp2d.device)
     for c in range(W):
         inpcol = array_api.reshape(inp2d[:, c].compact(), (H,))
